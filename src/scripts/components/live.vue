90ed22232c90c163835a9301271b63e71883a9ed
DeadlyZXP <1216706372@qq.com>
2016-10-29 21:26:47 +0800

    1.0.6加入live

--- src/scripts/components/live.vue
+++ src/scripts/components/live.vue
@@ -3,9 +3,7 @@
     <header class="liveBoxHeader">
       <span></span>
       <ul>
-        <li>直播</li>
-        <li>新帖</li>
-        <li>热帖</li>
+        <li v-for="l in liveList" v-on:click="liveSwiper($index)" v->{{l}}</li>
       </ul>
       <span></span>
     </header>
@@ -13,10 +11,47 @@
       <img src="/images/1.png" alt="">
     </nav>
     <section class="liveBoxSection">
-
+      <div class="swiper-container" id="index-swiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <section id="index-scroll">
+              <ul>
+                <li v-for="l in list" v-link="{name: 'detail', params: {id: l.id}}"><span><i><img v-bind:src="l.img" alt=""></i><b>{{l.title}}</b></span></li>
+              </ul>
+            </section>
+          </div>
+          <div class="swiper-slide" id="lifescroll">
+            <!-- <section id="waterfall"></section> -->
+          </div>
+          <div class="swiper-slide">
+            <!-- <section id="css3waterfall">
+              <div class="cont">
+                <div class="pic">
+                  <div v-for="item in beautylist">
+                    <span>
+                      <img v-bind:src="item.img" />
+                      <i v-html="item.title"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </section> -->
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 </template>
 <script>
+import commonUtil from "../utils/commonUtil";
+import waterfall from "../utils/waterfall.util.js";
 
+var mySwiper = null;
+export default {
+  data(){
+    return {
+      liveList:['直播','新帖','热帖']
+    }
+  }
+}
 </script>
