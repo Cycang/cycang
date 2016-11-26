ae4f7322aeae6984cec06a31e72506ec586cc5aa
snowfighting <songxj0130@126.com>
2016-11-26 16:47:35 +0800

    songxuejiao

---
+++ src/scripts/components/bianji.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bianji-container">
+    <header>
+    <div class="title-left">
+
+    </div>
+    <div class="title">
+      个人资料
+    </div>
+    <div class="title-right">
+      完成
+    </div>
+    </header>
+    <section>
+      <div class="name">
+        <p>昵称</p>
+        <input type="text" name="name" value="" v-module="" placeholder="请填写昵称">
+      </div>
+      <div class="youxiang">
+        <p>邮箱</p>
+        <input type="text" name="name" value="" v-module="" placeholder="请填写邮箱">
+      </div>
+      <div class="QQ">
+        <p>QQ</p>
+        <input type="text" name="name" value="" v-module="" placeholder="请填写QQ">
+      </div>
+    </section>
+  </div>
+</template>
---
+++ src/scripts/vuex/action.js
@@ -0,0 +1,4 @@
+export const classifyChanger = function({dispatch, state}, classifyIndex) {
+  dispatch('CLASSIFYCHANGETAB', classifyIndex);
+};
+//子路有引用入口
